ba6753c3fd475dbb29a6aeab5984f3cded348b27
Aravind <aravind1808@gmail.com>
2019-07-05 20:09:22 +0530

    Show of Products

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter, Route, Link,Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+import ProductPage from './components/productAdmin/0.product'
 
 class App extends React.Component{
     render(){
         return(
+            <BrowserRouter>
             <div>
                 <h1>Welcome to the App</h1>
+                <Link to ="/product">List Products</Link>
+                <Route path="/product" component={ProductPage} exact={true}/>
             </div>
+            </BrowserRouter>
         )
     }
 }
--- config/routes.js
+++ config/routes.js
@@ -5,17 +5,17 @@ const specificationController = require('../app/controller/1.specificationContro
 const userController = require('../app/controller/2.userController')
 const compareController = require('../app/controller/3.compareController')
 
-router.get('/products',productController.list)
-router.post('/products',productController.create)
-router.get('/products/:id', productController.show)
+router.get('/product',productController.list)
+router.post('/product',productController.create)
+router.get('/product/:id', productController.show)
 router.put('/product/:id',productController.update)
 router.delete('/product/:id',productController.destroy)
 
-router.get('/specifications', specificationController.list)
-router.post('/specifications', specificationController.create)
-router.get('/specifications/:id', specificationController.show)
-router.put('/specifications/:id', specificationController.update)
-router.delete('/specifications/:id', specificationController.destroy)
+router.get('/specification', specificationController.list)
+router.post('/specification', specificationController.create)
+router.get('/specification/:id', specificationController.show)
+router.put('/specification/:id', specificationController.update)
+router.delete('/specification/:id', specificationController.destroy)
 
 
 module.exports = router
