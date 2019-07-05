6b01af47b8d07405799cb82ad2a376bd356be3a7
Aravind <aravind1808@gmail.com>
2019-07-06 04:10:05 +0530

    Display Info Added

---
+++ client/src/components/productAdmin/3.specificationDisForm.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import axios from '../../config/axios'
+
+class SpecificationDisForm extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            screenSize: '',
+            screenResolution: '',
+            aspectRatio: '',
+            pixelDensity: '',
+            displayType: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e){
+        e.persist()
+        this.setState(() => ({
+            [e.target.name] : e.target.value
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <label>
+                        Screen Size <br />
+                        <input type = "number" value={this.state.screenSize} onChange = {this.handleChange} name = "screenSize" />
+                    </label><br />
+                    <label>
+                        Screen Resolution <br />
+                        <input type = "number" value={this.state.screenResolution} onChange = {this.handleChange} name = "screenResolution"/>                        
+                    </label><br/>
+                    <label>
+                        Aspect Ratio <br />
+                        <input type = "number" value={this.state.aspectRatio} onChange = {this.handleChange} name = "aspectRatio"/>
+                    </label><br />
+                    <label>
+                        Pixel Density <br />
+                        <input type = "number" value={this.state.pixelDensity} onChange = {this.handleChange} name = "pixelDensity"/>
+                    </label><br />
+                    <label>
+                        Display Type <br />
+                        <input type = "text" value={this.state.displayType} onChange = {this.handleChange} name = "displayType"/>
+                    </label><br />
+                </form>
+            </div>            
+        )
+    }
+}
+
+export default SpecificationDisForm
--- client/src/components/stepZilla.js
+++ client/src/components/stepZilla.js
@@ -4,12 +4,14 @@ import StepZilla from 'react-stepzilla'
 import ProductForm from './productAdmin/0.productForm'
 import SpecificationGeneralForm from './productAdmin/1.specificationGeneralForm'
 import SpecificationDimForm from './productAdmin/2.specificationDimForm'
+import SpecificationDisForm from './productAdmin/3.specificationDisForm'
 
 const steps =
     [
       {name: 'Product Add',component: <ProductForm />},
       {name: 'General Specification',component: <SpecificationGeneralForm />},
-      {name: 'Dimension Specification',component: <SpecificationDimForm />}
+      {name: 'Dimension Specification',component: <SpecificationDimForm />},
+      {name: 'Display Specification',component: <SpecificationDisForm />},
     ]
 
 class Steps extends React.Component {
