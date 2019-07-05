5379e0a85a5b038f9f0a9011532dd534654121bf
Aravind <aravind1808@gmail.com>
2019-07-05 14:12:48 +0530

    delete Path was missing

--- config/routes.js
+++ config/routes.js
@@ -9,5 +9,6 @@ router.get('/product',productController.list)
 router.get('/product/:id',productController.show)
 router.post('/product',productController.create)
 router.put('/product/:id',productController.update)
+router.delete('/product/:id',productController.destroy)
 
 module.exports = router
