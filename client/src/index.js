8796e200588b6228f0b917da8cb88e7f0efd31be
Aravind <aravind1808@gmail.com>
2019-07-06 14:47:16 +0530

    Add seperate page made

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter, Route, Link, Switch} from 'react-router-dom'
 
-import Steps from './components/stepZilla'
+import AddPage from './components/AddPage'
 import ProductShow from './components/productAdmin/productShow'
 
 class App extends React.Component{
@@ -12,10 +12,10 @@ class App extends React.Component{
             <BrowserRouter>
             <div>
                 <h1>Welcome to the App</h1>
-                <Link to ="/products"><h2>Add Product</h2></Link>
-                <Link to="products/show"><h2>Show Product(added by postman)</h2></Link>
+                <Link to ="/products"><h2>Add</h2></Link>
+                <Link to="products/show"><h2>Show(added by postman)</h2></Link>
                 <Switch>
-                    <Route path="/products" component={Steps} exact={true}/>
+                    <Route path="/products" component={AddPage} exact={true}/>
                     <Route path="/products/show" component={ProductShow} />
                 </Switch>
             </div>
